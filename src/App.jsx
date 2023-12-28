7d9e189500a3e54494baacdcc6c94ae337f90895
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2023-12-28 12:55:15 +0530

    learnt using state for accessing the value of an input tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,26 @@ import Todo from "./components/todos"
 
 export default function App(){
   // get the items from network call
-  const todoList = [
-    {
-      task:"eat tomato",
-      date: "11-12-23"
-    },
-    {
-      task:"run daily",
-      date: "Daily"
-    },
-    {
-      task:"kill mosquitos",
-      date: "daily"
-    },
-    {
-      task:"start learning react",
-      date: "11-11-23"
-    }
+  // const todoList = [
+  //   {
+  //     task:"eat tomato",
+  //     date: "11-12-23"
+  //   },
+  //   {
+  //     task:"run daily",
+  //     date: "Daily"
+  //   },
+  //   {
+  //     task:"kill mosquitos",
+  //     date: "daily"
+  //   },
+  //   {
+  //     task:"start learning react",
+  //     date: "11-11-23"
+  //   }
     
-  ]
+  // ]
+  const todoList = [];
 
   return(
     <div className="container pt-4">
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,30 +1,72 @@
 /* eslint-disable react/prop-types */
-function Todo(props){
-    return <div className="container">
-        <div className="row mb-3 mt-3">
+import { useState } from "react";
+
+function Todo({todoList}){
+
+    const [todoArr,setTodoArr] = useState(todoList);
+
+    function handleDelete(item,event){
+        console.log(`deleted task ${item}`);
+        event.target.parentElement.parentElement.style.backgroundColor = "black"
+        event.target.parentElement.parentElement.style.color = "white"
+    }
+
+    function handleAddTaskClick(){
+        // accessing the inut val 👇
+        // console.log("your task is ",taskVal);
+        // console.log("your date is ",dateVal);
+        
+        if(taskVal === "" || dateVal === "" ) return;
+        // creating a new taskArr
+        const newTask = {
+            task: taskVal,
+            date: dateVal
+        }
+        const newTodoArr = [...todoArr,newTask]
+        setTodoArr(newTodoArr);
+        // change the vals of input 
+        setTaskVal('')
+        setDateVal('')
+
+    }
+
+    // grabbing input tag values
+    // task input
+    const [taskVal,setTaskVal] = useState("");
+    const [dateVal,setDateVal] = useState("");
+    let content ;
+    if(todoArr.length===0) content = <h1>No items found</h1>
+    else content = todoArr.map((elem,i)=>(
+        <div className="row mb-3" key={i}>
             <div className="col">
-                <input type="text" name="todo" placeholder="Enter your task" />
+                {elem.task}
             </div>
             <div className="col">
-                <input type="date" name="date" />
+                {elem.date}
             </div>
             <div className="col">
-                <button type="button" className="btn btn-primary">Add task</button>
+                <button type="button" className="btn btn-danger" onClick={(evnt)=>(handleDelete(elem.task,evnt))}>delete</button>
             </div>
         </div>
-        {props.todoList.map((e,i)=>(
-            <div className="row mb-3" key={i}>
-                <div className="col">
-                    {e.task}
-                </div>
-                <div className="col">
-                    {e.date}
-                </div>
-                <div className="col">
-                    <button type="button" className="btn btn-danger">delete</button>
-                </div>
+    ))
+
+
+    return <div className="container">
+        {/* to do adder row */}
+        <div className="row mb-3 mt-3">
+            <div className="col">
+                <input type="text" value={taskVal} placeholder="Enter your task" onChange={(e)=>setTaskVal(e.target.value)} />
             </div>
-        ))}
+            <div className="col">
+                <input type="date" value={dateVal} onChange={(e)=>setDateVal(e.target.value)} />
+            </div>
+            <div className="col">
+                <button type="button" className="btn btn-primary" onClick={handleAddTaskClick} >Add task</button>
+            </div>
+        </div>
+
+        {/* todos */}           
+        {content}
     </div>
 }
 
