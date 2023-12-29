1da31b77a0e5be9023b2aff76d8577e88682d865
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2023-12-29 23:30:07 +0530

    learnt useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import Header from './components/header'
+import Header from './components/0. header'
 // import Todo from "./components/todos"
-import TodoWithForm from "./components/todosForm";
+// import TodoWithForm from "./components/todosForm";
+import TodoUseRef from "./components/3. todosUseRef";
 
 export default function App(){
   // get the items from network call
   // const todoList = [
-  //   {
+  //  {
   //     task:"eat tomato",
   //     date: "11-12-23"
-  //   },
-  //   {
-  //     task:"run daily",
-  //     date: "Daily"
-  //   },
-  //   {
-  //     task:"kill mosquitos",
-  //     date: "daily"
-  //   },
-  //   {
-  //     task:"start learning react",
-  //     date: "11-11-23"
   //   }
-    
   // ]
   const todoList = [];
 
@@ -30,7 +18,8 @@ export default function App(){
     <div className="container pt-4">
       <Header/>
       {/* <Todo todoList={todoList}/> */}
-      <TodoWithForm todoList={todoList}/>
+      {/* <TodoWithForm todoList={todoList}/> */}
+      <TodoUseRef todoList={todoList}/>
     </div>
   )
 }
